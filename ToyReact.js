667b5e60adbeaace71f55b59a38455e23785ece6
red0908 <332673227@qq.com>
2020-08-04 22:42:13 +0800

    feat: 添加对应的生命周期，实现动态修改内容的功能

--- ToyReact.js
+++ ToyReact.js
@@ -4,43 +4,93 @@ class ElementWrapper {
     this.root = document.createElement(type)
   }
   setAttribute (key, value) {
+    // \s为空白 \S为非空白，加一起为所有字符
+    if (key.match(/^on([\s\S]+)$/)) {
+        let eventName = RegExp.$1.replace(/^[s\S]/, s => s.toLowerCase())
+        this.root.addEventListener(eventName, value)
+    }
+    if (key === 'className')
+      key = 'class'
     this.root.setAttribute(key, value)
   }
   appendChild (vChild) {
-    vChild.mountTo(this.root)
+    let range = document.createRange()
+    if (this.root.children.length) {
+      range.setStartAfter(this.root.lastChild)
+      range.setEndAfter(this.root.lastChild)
+    } else {
+      range.setStart(this.root,0)
+      range.setEnd(this.root, 0)
+    }
+    vChild.mountTo(range)
   }
-  mountTo (parent) {
-    parent.appendChild(this.root)
+  mountTo (range) {
+    range.deleteContents()
+    range.insertNode(this.root)
   }
 }
 class TextWrapper {
   constructor (type) {
     this.root = document.createTextNode(type)
   }
-  mountTo (parent) {
-    parent.appendChild(this.root)
+  mountTo (range) {
+    range.deleteContents()
+    range.insertNode(this.root)
   }
 }
 // wrapper让Component和原生的dom行为一致
 export class Component {
   constructor () {
-    this.chrildren = []
+    this.children = []
+    this.props = Object.create(null)
   }
   setAttribute (key, value) {
+    this.props[key] = value
     this[key] = value
   }
-  mountTo (parent) {
+  mountTo (range) {
+    this.range = range
+    this.update()
+  }
+  update () {
+    let placeholder = document.createComment('placeholder')
+    let range = document.createRange()
+    range.setStart(this.range.endContainer, this.range.endOffset)
+    range.setEnd(this.range.endContainer, this.range.endOffset)
+    range.insertNode(placeholder)
+    this.range.deleteContents()
     let vdom = this.render()
-    vdom.mountTo(parent)
+    vdom.mountTo(this.range)
+
+    placeholder.parentNode.removeChild(placeholder)
   }
   appendChild (vChild) {
-    this.chrildren.push(vChild)
+    this.children.push(vChild)
+  }
+  setState (state) {
+    let merge = (oldState, newState) => {
+      for (let p in newState) {
+        if (typeof newState[p] === "object") {
+          if (typeof oldState[p] !== "object") {
+            oldState[p] = {}
+          } 
+          merge(oldState[p], newState[p])
+        } else {
+          oldState[p] = newState[p]
+        }
+      }
+    }
+    if (!this.state && state)
+      this.state = {}
+    merge(this.state, state)
+    console.log(this.state);
+    this.update()
   }
 }
 
 
 export let ToyReact = {
-  createElement (type, attribute, ...chrildren) {
+  createElement (type, attribute, ...children) {
     let element;
     if (typeof type === 'string')
       element = new ElementWrapper(type)
@@ -51,10 +101,10 @@ export let ToyReact = {
       element.setAttribute(key,attribute[key])
     }
 
-    let insertchrildren = (chrildren) => {
-      chrildren.forEach(child => {
+    let insertchildren = (children) => {
+      children.forEach(child => {
         if (typeof child === 'object' && child instanceof Array) {
-          insertchrildren(child)
+          insertchildren(child)
         } else {
           if (!(child instanceof Component)
           && !(child instanceof ElementWrapper)
@@ -68,13 +118,21 @@ export let ToyReact = {
         }
       })
     }
-    insertchrildren(chrildren)
+    insertchildren(children)
 
     return element
   },
   render (vdom, element) {
+    let range = document.createRange()
+    if (element.children.length) {
+      range.setStartAfter(element.lastChild)
+      range.setEndAfter(element.lastChild)
+    } else {
+      range.setStart(element,0)
+      range.setEnd(element, 0)
+    }
     //递归调用
-    vdom.mountTo(element)
+    vdom.mountTo(range)
     // document.appendChild(vdom)
   }
 }
---
+++ game.js
@@ -0,0 +1,75 @@
+import {ToyReact, Component} from './ToyReact.js'
+
+class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+  render() {
+    return (
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value ? this.state.value : ""}
+      </button>
+    );
+  }
+}
+class Board extends Component {
+  renderSquare(i) {
+    return (
+      <Square value={i}/>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+let a = <Board/>;
+
+
+ToyReact.render(
+  a,
+  document.body
+)
+
+
+/**
+ * a被翻译成了下面的代码
+ * 
+ var a = ToyReact.createElement("div", {
+  id: "a",
+  name: "a"
+}, ToyReact.createElement("span", null, "hello "), 
+ToyReact.createElement("span", null, "world"), 
+ToyReact.createElement("span", null, "!"));
+
+类比于react的
+return React.createElement('div', {className: 'shopping-list'},
+  React.createElement('h1', ... h1 children ... ),
+  React.createElement('ul', ... ul children ... )
+);
+*/
+/**
+document.body.appendChild(a)
+*/
